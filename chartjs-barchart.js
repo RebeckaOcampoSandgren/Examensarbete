aae68034af87c34dca6a6bf56b13bc3182a05de2
a19rebsa <62889999+a19rebsa@users.noreply.github.com>
2022-03-28 13:57:00 +0200

    Added filtering
    Added interaction in form of filtering on http method type

--- chartjs-barchart.js
+++ chartjs-barchart.js
@@ -1,13 +1,14 @@
 let ctx = document.getElementById('barChart').getContext('2d');
+let data, chart;
+let methodCount = {};
 
 $.getJSON("logfile.json", function (json) {
-    //Generate array with https methods 
-    let data = json.map(function (e) {
+    //Generate array with https methods from log file
+    data = json.map(function (e) {
         return e.method;
     });
 
     //Count occurrence of every http method in data array and add method to object with occurrence as value
-    let methodCount = {};
     for (let element of data) {
         if (methodCount[element]) {
             methodCount[element] += 1;
@@ -28,8 +29,27 @@ $.getJSON("logfile.json", function (json) {
         }
     };
 
-    var chart = new Chart(ctx, dataObj);
+    chart = new Chart(ctx, dataObj);
 });
 
+//Get input from checkboxes and update chart with chosen methods
+function updateChart() {
+    let selectedMethods = {};
+    let checkboxValues = [];
+    const checkboxes = document.getElementsByClassName('methodCheckbox');
 
+    for (let checkbox of checkboxes) {
+        if (checkbox.checked) {
+            checkboxValues.push(checkbox.value.toUpperCase());
+        }
+    }
+
+    for (let value of checkboxValues) {
+        selectedMethods[value] = methodCount[value];
+    }
+
+    chart.data.labels = Object.keys(selectedMethods);
+    chart.data.datasets[0].data = Object.values(selectedMethods);
+    chart.update();
+}
 
