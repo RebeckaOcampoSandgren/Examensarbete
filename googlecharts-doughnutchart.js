f9118efb348ffcc1c7cef1346b20fa7fde30c4da
a19rebsa <62889999+a19rebsa@users.noreply.github.com>
2022-04-11 11:27:58 +0200

    Add filter functionality
    Added filter for status classes

--- googlecharts-doughnutchart.js
+++ googlecharts-doughnutchart.js
@@ -1,13 +1,23 @@
 let statusArr = [/^1/, /^2/, /^3/, /^4/, /^5/];
 let statusLabel = ["Informational", "Successful", "Redirectional", "Client Error", "Server Error"];
 let counts = {};
+var filterbtn = document.getElementById('filter');
+let chart, datajson;
+//set options for chart
+let options = {
+    title: 'HTTP Status Distribution',
+    'width': 1300,
+    'height': 600,
+    pieHole: 0.4,
+    pieSliceText: 'value',
+    legend: { position: 'top' },
+};
 
 // Load the Visualization API and the piechart package.
 google.charts.load("current", { packages: ["corechart"] });
 // Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChart());
 
-let chart, data, datajson;
 
 function drawChart() {
     $.getJSON("logfile1.json", function (json) {
@@ -26,23 +36,32 @@ function drawChart() {
                 }
             }
         }
-
-        var data = new google.visualization.DataTable();
+        //Create data table
+        let data = new google.visualization.DataTable();
         data.addColumn('string', 'statusclass');
         data.addColumn('number', 'count');
         data.addRows(Object.entries(counts));
 
-        var options = {
-            title: 'HTTP Status Distribution',
-            'width': 1300,
-            'height': 600,
-            pieHole: 0.4,
-            pieSliceText: 'value',
-            legend: { position: 'top' },
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+        chart = new google.visualization.PieChart(document.getElementById('donutchart'));
         chart.draw(data, options);
-
     });
+
+}
+
+//Get input from checkboxes and update chart with chosen status classes
+function updateChart() {
+    let selectedStatus = {};
+    let checkboxes = document.getElementsByClassName('statusCheckbox');
+
+    for (let checkbox of checkboxes) {
+        if (checkbox.checked) {
+            selectedStatus[checkbox.value] = counts[checkbox.value];
+        }
+    }
+
+    let updateData = new google.visualization.DataTable();
+    updateData.addColumn('string', 'statusclass');
+    updateData.addColumn('number', 'count');
+    updateData.addRows(Object.entries(selectedStatus));
+    chart.draw(updateData, options);
 }
